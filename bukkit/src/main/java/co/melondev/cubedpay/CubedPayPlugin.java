50f9697a51a6369093bf784e84e8ce1f4c477262
Cameron Weaver (Clutch) <lockerzmodding@gmail.com>
2018-04-03 21:08:01 -0400

    Bukkit plugin created

--- bukkit/src/main/java/co/melondev/cubedpay/CubedPayPlugin.java
+++ bukkit/src/main/java/co/melondev/cubedpay/CubedPayPlugin.java
@@ -1,5 +1,57 @@
 package co.melondev.cubedpay;
 
-public class CubedPayPlugin {
+import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
+public class CubedPayPlugin extends JavaPlugin {
+
+    private CubedPayAPI api;
+    private CubedPaySettings settings = new CubedPaySettings();
+
+    @Override
+    public void onEnable() {
+        File dir = getDataFolder();
+        if (!dir.exists()) dir.mkdir();
+
+        File config = new File(dir, "config.yml");
+        if (!config.exists()) saveDefaultConfig();
+
+        settings.load(getConfig());
+
+        boolean disable = false;
+        if (settings.getAppID() == null || settings.getAppID().isEmpty()) {
+            getLogger().severe("AppID is blank in the config!");
+            disable = true;
+        }
+        if (settings.getAccessToken() == null || settings.getAccessToken().isEmpty()) {
+            getLogger().severe("AccessToken is blank in the config!");
+            disable = true;
+        }
+        if (disable) {
+            getLogger().severe("CubedPayPlugin will now disable.");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        api = CubedPayAPI.create(settings.getAppID(), settings.getAccessToken());
+
+        if (settings.getShopID() == null || settings.getShopID().isEmpty()) {
+            getLogger().severe("ShopID is blank in the config! Not starting event runnable!");
+            return;
+        }
+        api.startEvents(settings.getShopID());
+    }
+
+    public static CubedPayAPI getAPI() {
+        return getInstance().api;
+    }
+
+    public static CubedPaySettings getSettings() {
+        return getInstance().settings;
+    }
+
+    private static CubedPayPlugin getInstance() {
+        return CubedPayPlugin.getPlugin(CubedPayPlugin.class);
+    }
 }
