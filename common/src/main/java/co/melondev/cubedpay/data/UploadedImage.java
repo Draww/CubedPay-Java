e3b983f918e547ecd6fb8ea6f380be988c90d0cc
theminecoder <buisness@theminecoder.me>
2018-08-21 10:39:57 +0100

    File download support

--- common/src/main/java/co/melondev/cubedpay/data/UploadedImage.java
+++ common/src/main/java/co/melondev/cubedpay/data/UploadedImage.java
@@ -1,5 +1,15 @@
 package co.melondev.cubedpay.data;
 
+import com.google.common.base.Joiner;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.util.Arrays;
+
 public class UploadedImage {
 
     private String id = "";
@@ -11,6 +21,22 @@ public class UploadedImage {
 
     public String getUrl() {
         return url;
+    }
+
+    public void download(File file) throws IOException {
+        ReadableByteChannel rbc = Channels.newChannel(new URL(url).openStream());
+        FileOutputStream fos = new FileOutputStream(file);
+        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        fos.close();
+    }
+
+    public File downloadToTempFile() throws IOException {
+        String[] nameSplit = url.substring(url.lastIndexOf("/")).split("\\.");
+        File file = File.createTempFile(
+                "cubedpay-" + Joiner.on(".").join(Arrays.copyOfRange(nameSplit, 0, nameSplit.length - 2)),
+                nameSplit[nameSplit.length - 1]);
+        download(file);
+        return file;
     }
 
     @Override
